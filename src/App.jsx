540c7a65923a057426dadbc018f01e4450380fdc
Sergio Cardona <chechocardona@gmail.com>
2023-07-25 10:00:41 -0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import styled from '@emotion/styled'
+
+const Heading = styled.h1`
+  font-family: lato;
+  color: #fff;
+`;
+
+function App() {
+  return (
+    <Heading>Desde App</Heading>      
+  )
+}
+
+export default App
