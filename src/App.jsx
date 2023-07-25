df6d626b2d059ef65ca2dfadf19fff9df78f2344
Sergio Cardona <chechocardona@gmail.com>
2023-07-25 12:59:59 -0500

    Added Container, Image and modified Heading

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,57 @@
 import { useState } from 'react'
 import styled from '@emotion/styled'
+import ImagenCripto from './img/imagen-criptos.png'
+
+
+const Contenedor = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  width: 90%;
+  @media screen and (min-width: 992px){
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+`
 
 const Heading = styled.h1`
   font-family: lato;
   color: #fff;
-`;
+  text-align: center;
+  font-weight: 700;
+  font-size: 34px;
+  margin-top: 80px;
+  margin-bottom: 50px;
+
+  &::after{
+    content: '';
+    width: 100px;
+    height: 6px;
+    background-color: #66a2fe;
+    display: block;
+    margin: 10px auto 0 auto;
+  }
+`
+
+const Imagen = styled.img`
+  max-width: 400px;
+  width: 80%;
+  margin: 100px auto 0 auto;
+  display: block;
+`
 
 function App() {
   return (
-    <Heading>Desde App</Heading>      
+    <Contenedor>
+      <Imagen
+        src={ImagenCripto}
+        alt='Imagen de Criptos'
+      />
+      <div>
+        <Heading>Cotiza Criptomonedas al instante</Heading>
+      </div>
+    </Contenedor>
+          
   )
 }
 
