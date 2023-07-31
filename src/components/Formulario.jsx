41343438327026c397a1a83aeb0857aa5eff412e
Sergio Cardona <chechocardona@gmail.com>
2023-07-31 15:31:56 -0500

    Added custom Hook to generate a select with a list of currencies, styled the compoents

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import styled from '@emotion/styled'
+import useSelectMonedas from '../hooks/useSelectMonedas'
+import { monedas } from '../data/monedas'
 
 
 const InputSubmit = styled.input`
@@ -13,6 +15,7 @@ const InputSubmit = styled.input`
     font-size: 20px;
     border-radius: 5px;
     transition: background-color .3s ease;
+    margin-top: 30px;
 
     &:hover{
         background-color: #7A7DFE;
@@ -21,12 +24,22 @@ const InputSubmit = styled.input`
 `
 
 const Formulario = () => {
+
+  const [ SelectMonedas ] = useSelectMonedas('Elije tu Moneda', monedas)
+  
   return (
-    <InputSubmit
+
+    <form>
+
+      <SelectMonedas/>
+
+      <InputSubmit
         type='submit'
         value='cotizar'
         
-    />
+      />
+
+    </form>
   )
 }
 
